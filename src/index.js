d82936f6f0e614847b45b63a7397839ff02e8f92
RomanAbdalov <117114038+RomanAbdalov@users.noreply.github.com>
2023-04-22 14:46:50 +0300

    add async function onSubmitForm

--- src/index.js
+++ src/index.js
@@ -18,7 +18,64 @@ const onloadMoreButton = new LoadMoreButton({
     loading: false,
     buttonAdress: refs.loadMoreButton
 });
+let totalImagesUploaded = 40;
 
+onloadMoreButton.buttonState({});
+
+refs.form.addEventListener('submit', onSubmitForm);
+refs.loadMoreButton.addEventListener('click', onSumbitLoadMore);
+
+async function onSubmitForm(event) {
+    event.preventDefault();
+    const { searchQuery } = event.currentTarget.elements;
+    if (!searchQuery.value.trim()) {
+        Notify.info('Please, enter data to search!');
+        return;
+    }
+refs.searchButton.disabled = true;
+requestServer.params.page = 0;
+refs.gallary.innerHTML = '';
+onloadMoreButton.buttonState({
+    isHiden: false,
+    loading: true,
+});
+
+try {
+    const response = await requestServer.onRequestServer(searchQuery.value);
+    const { hits, totalHits } = response.data;
+
+    if(!totalHits) {
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    onloadMoreButton.buttonState({isHiden: true});
+
+    refs.searchButton.disabled = false;
+    return;
+    }
+
+    Notify.success(`Hooray! We found ${totalHits} images.`)
+    refs.gallery.insertAdjacentHTML("beforeend", markup(hits));
+    refs.searchButton.disabled = false;
+    
+
+    const lightbox = new SimpleLightbox('.gallery a');
+    lightbox.refresh();
+
+    if (totalHits <= 40) {
+        onloadMoreButton.buttonState({
+            isHiden: true,
+            disabled: true,
+        })
+    } else {
+        onloadMoreButton.buttonState({
+            isHiden: false,
+            disabled: false,
+            loading: false,
+        });
+    }
+} catch(error) {
+    console.log(error);
+};
+};
 
 
 
